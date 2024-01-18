8d57845d9c198dd1ea83b19def6ac9d1d772a3c9
Jai Radhakrishnan <55522316+jairad26@users.noreply.github.com>
2024-01-17 22:20:04 -0800

    updates

--- src/assembly/hypermode.ts
+++ src/assembly/hypermode.ts
@@ -1,5 +1,5 @@
 // This file should only export functions from the "hypermode" host module.
 
 export declare function executeDQL(statement: string, isMutation: bool): string;
-export declare function executeGQL(statement: string, variables: Map<string, string>): string;
+export declare function executeGQL(statement: string, variables: string): string;
 export declare function invokeClassifier(modelId: string, sentence: string): string;
--- src/assembly/index.ts
+++ src/assembly/index.ts
@@ -23,7 +23,8 @@ export abstract class dql {
 
 export abstract class graphql {
     static execute<TData>(statement: string, variables: Map<string, string> = new Map<string, string>()): GQLResponse<TData> {
-        const response = host.executeGQL(statement, variables);
+        let variablesJson = JSON.stringify<Map<string, string>>(variables);
+        const response = host.executeGQL(statement, variablesJson);
         return JSON.parse<GQLResponse<TData>>(response);
     }
 }
