6e234df62840a2f4044849c235664ec7ab217b8f
Matt Johnson-Pint <mjp@hypermode.com>
2024-04-03 11:40:25 -0700

    Improve build output

--- src/transform/src/index.ts
+++ src/transform/src/index.ts
@@ -1,7 +1,7 @@
 import { Transform } from "assemblyscript/dist/transform.js";
+import { createWriteStream } from "fs";
 import { HypermodeMetadata } from "./metadata.js";
 import { Extractor } from "./extractor.js";
-import writeLogo from "./logo.js";
 
 export default class HypermodeTransform extends Transform {
   async afterCompile(module) {
@@ -12,7 +12,15 @@ export default class HypermodeTransform extends Transform {
     m.addFunctions(functions);
     m.writeToModule(module);
 
-    writeLogo(process.stdout);
+    // Write to stdout
     m.logToStream(process.stdout);
+
+    // If running in GitHub Actions, also write to the step summary
+    if (process.env.GITHUB_ACTIONS && process.env.GITHUB_STEP_SUMMARY) {
+      const stream = createWriteStream(process.env.GITHUB_STEP_SUMMARY, {
+        flags: "a",
+      });
+      m.logToStream(stream, true);
+    }
   }
 }
