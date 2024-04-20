c7bfd54dbc2d596fe3158629ba07985a85406e0a
Jai Radhakrishnan <55522316+jairad26@users.noreply.github.com>
2024-04-19 20:49:55 -0700

    update mock tests

--- src/assembly/__tests__/dql.spec.ts
+++ src/assembly/__tests__/dql.spec.ts
@@ -3,7 +3,7 @@ import { DQLResponse } from "../types/dqltypes";
 
 describe("DQL Host Functions", () => {
   it("can query DQL", () => {
-    const response = dql.query<string>("ping");
+    const response = dql.query<string>("ping", "pong");
     const expected = new DQLResponse<string>();
     expected.data = "pong";
     expect(response).toStrictEqual(expected);
