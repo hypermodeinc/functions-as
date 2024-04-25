e01e279e0503f28226325552f39187cdab855475
Matt Johnson-Pint <mjp@hypermode.com>
2024-04-25 00:30:58 -0700

    Update gqltypes.ts

--- src/assembly/types/gqltypes.ts
+++ src/assembly/types/gqltypes.ts
@@ -1,50 +1,25 @@
+import { JSON } from "json-as";
+
 
 @json
 export class GQLResponse<T> {
-  data!: T;
-  extensions: GQLExtensions | null = null;
-  // TODO: errors
+  errors: GQLError[] | null = null;
+  data: T | null = null;
+  // extensions: Map<string, ???> | null = null;
 }
 
 
 @json
-class GQLExtensions {
-  touched_uids: u32 = 0;
-  tracing!: GQLTracing;
+class GQLError {
+  message!: string;
+  locations: GQLErrorLocation[] | null = null;
+  path: string[] | null = null;
+  // extensions: Map<string, ???> | null = null;
 }
 
 
 @json
-class GQLTracing {
-  version!: u32;
-  startTime!: Date;
-  endTime!: Date;
-  duration!: u32;
-  execution: GQLExecution | null = null;
-}
-
-
-@json
-class GQLExecution {
-  resolvers!: GQLResolver[];
-}
-
-
-@json
-class GQLResolver {
-  path!: string[];
-  parentType!: string;
-  fieldName!: string;
-  returnType!: string;
-  startOffset!: u32;
-  duration!: u32;
-  dgraph!: GQLDgraph[];
-}
-
-
-@json
-class GQLDgraph {
-  label!: string;
-  startOffset!: u32;
-  duration!: u32;
+class GQLErrorLocation {
+  line!: u32;
+  column!: u32;
 }
