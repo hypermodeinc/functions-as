df46943fffa281ee29e955b49e4142aafc4635fe
Matt Johnson-Pint <mjp@hypermode.com>
2024-05-22 11:10:17 -0700

    Remove extraneous JSON import (#91)

--- examples/textgeneration/assembly/product.ts
+++ examples/textgeneration/assembly/product.ts
@@ -1,8 +1,3 @@
-// For now, this import is required when we use @json, even if we're not calling
-// methods on the JSON class.  It will be fixed in the future.
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { JSON } from "json-as";
-
 // The Product class and the sample product will be used in the some of the examples.
 // Note that the class must be decorated with @json so that it can be serialized
 // and deserialized properly when interacting with OpenAI.
