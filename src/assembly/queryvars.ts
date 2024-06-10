5af66068fdf4745c5f9c40be93f7f02576307140
Matt Johnson-Pint <mjp@hypermode.com>
2024-06-10 12:43:51 -0700

    Support query vars of any serializable type

--- src/assembly/queryvars.ts
+++ src/assembly/queryvars.ts
@@ -1,113 +1,21 @@
-import { Variant } from "as-variant/assembly";
 import { JSON } from "json-as";
 
 export class QueryVariables {
-  private variables: Map<string, Variant> = new Map<string, Variant>();
+  private data: Map<string, string> = new Map<string, string>();
 
   public set<T>(name: string, value: T): void {
-    this.variables.set(name, Variant.from(value));
+    this.data.set(name, JSON.stringify(value));
   }
 
   public toJSON(): string {
-    /*
-    The following would be ideal but is not currently supported:
-      
-        return JSON.stringify(this.variables);
-    
-    See https://github.com/JairusSW/as-json/issues/64
-    Instead, we have to manually create a JSON string.
-    */
     const segments: string[] = [];
+    const keys = this.data.keys();
+    const values = this.data.values();
 
-    const keys = this.variables.keys();
-    const values = this.variables.values();
-
-    for (let i = 0; i < this.variables.size; i++) {
+    for (let i = 0; i < this.data.size; i++) {
       const key = JSON.stringify(keys[i]);
-      const value = values[i];
-
-      switch (value.id) {
-        // Simple types
-        case Variant.idof<string>():
-          segments.push(`${key}:${JSON.stringify(value.get<string>())}`);
-          break;
-        case Variant.idof<bool>():
-          segments.push(`${key}:${JSON.stringify(value.get<bool>())}`);
-          break;
-        case Variant.idof<f64>():
-          segments.push(`${key}:${JSON.stringify(value.get<f64>())}`);
-          break;
-        case Variant.idof<f32>():
-          segments.push(`${key}:${JSON.stringify(value.get<f32>())}`);
-          break;
-        case Variant.idof<i64>():
-          segments.push(`${key}:${JSON.stringify(value.get<i64>())}`);
-          break;
-        case Variant.idof<i32>():
-          segments.push(`${key}:${JSON.stringify(value.get<i32>())}`);
-          break;
-        case Variant.idof<i16>():
-          segments.push(`${key}:${JSON.stringify(value.get<i16>())}`);
-          break;
-        case Variant.idof<i8>():
-          segments.push(`${key}:${JSON.stringify(value.get<i8>())}`);
-          break;
-        case Variant.idof<u64>():
-          segments.push(`${key}:${JSON.stringify(value.get<u64>())}`);
-          break;
-        case Variant.idof<u32>():
-          segments.push(`${key}:${JSON.stringify(value.get<u32>())}`);
-          break;
-        case Variant.idof<u16>():
-          segments.push(`${key}:${JSON.stringify(value.get<u16>())}`);
-          break;
-        case Variant.idof<u8>():
-          segments.push(`${key}:${JSON.stringify(value.get<u8>())}`);
-          break;
-
-        // Arrays of simple types
-        case Variant.idof<string[]>():
-          segments.push(`${key}:${JSON.stringify(value.get<string[]>())}`);
-          break;
-        case Variant.idof<bool[]>():
-          segments.push(`${key}:${JSON.stringify(value.get<bool[]>())}`);
-          break;
-        case Variant.idof<f64[]>():
-          segments.push(`${key}:${JSON.stringify(value.get<f64[]>())}`);
-          break;
-        case Variant.idof<f32[]>():
-          segments.push(`${key}:${JSON.stringify(value.get<f32[]>())}`);
-          break;
-        case Variant.idof<i64[]>():
-          segments.push(`${key}:${JSON.stringify(value.get<i64[]>())}`);
-          break;
-        case Variant.idof<i32[]>():
-          segments.push(`${key}:${JSON.stringify(value.get<i32[]>())}`);
-          break;
-        case Variant.idof<i16[]>():
-          segments.push(`${key}:${JSON.stringify(value.get<i16[]>())}`);
-          break;
-        case Variant.idof<i8[]>():
-          segments.push(`${key}:${JSON.stringify(value.get<i8[]>())}`);
-          break;
-        case Variant.idof<u64[]>():
-          segments.push(`${key}:${JSON.stringify(value.get<u64[]>())}`);
-          break;
-        case Variant.idof<u32[]>():
-          segments.push(`${key}:${JSON.stringify(value.get<u32[]>())}`);
-          break;
-        case Variant.idof<u16[]>():
-          segments.push(`${key}:${JSON.stringify(value.get<u16[]>())}`);
-          break;
-        case Variant.idof<u8[]>():
-          segments.push(`${key}:${JSON.stringify(value.get<u8[]>())}`);
-          break;
-
-        default:
-          throw new Error(
-            "Query variables must be simple types, or arrays of simple types.",
-          );
-      }
+      const value = values[i]; // already in JSON
+      segments.push(`${key}:${value}`);
     }
 
     return `{${segments.join(",")}}`;
