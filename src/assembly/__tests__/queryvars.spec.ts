83656cac4410efb38c1bf4a4ada7b46b79ebb558
Matt Johnson-Pint <mjp@hypermode.com>
2024-06-10 13:16:25 -0700

    Support complex types as variables to external GraphQL query (#99)

--- src/assembly/__tests__/queryvars.spec.ts
+++ src/assembly/__tests__/queryvars.spec.ts
@@ -8,11 +8,20 @@ describe("Query Variables Object", () => {
     vars.set("key3", true);
     vars.set("key4", [123, 456, 789]);
     vars.set("key5", [1.234, 2.345, 3.456]);
+    vars.set("key6", <Foo>{ a: 1, b: 2, c: 3 });
 
     const result = vars.toJSON();
 
-    const expected = `{"key1":"abc","key2":123,"key3":true,"key4":[123,456,789],"key5":[1.234,2.345,3.456]}`;
+    const expected = `{"key1":"abc","key2":123,"key3":true,"key4":[123,456,789],"key5":[1.234,2.345,3.456],"key6":{"a":1,"b":2,"c":3}}`;
 
     expect(result).toBe(expected);
   });
 });
+
+
+@json
+class Foo {
+  a!: i32;
+  b!: i32;
+  c!: i32;
+}
