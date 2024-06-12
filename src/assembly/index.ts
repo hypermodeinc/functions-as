ffd8067e408a2db9822f52c2f482d756805c6fb9
Matt Johnson-Pint <mjp@hypermode.com>
2024-06-12 12:16:07 -0700

    Fix console global quirk

--- src/assembly/index.ts
+++ src/assembly/index.ts
@@ -4,7 +4,3 @@ export * from "./connection";
 
 import * as http from "./http";
 export { http };
-
-// Overrides are referenced from the plugin.asconfig.json file.
-// They need to be imported here so the compiler doesn't optimize them out.
-import * as _ from "./overrides";
