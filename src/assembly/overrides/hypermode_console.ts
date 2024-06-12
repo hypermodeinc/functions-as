c7510a18546abfbab3e464c0e5b5ad2e202a2bbf
Matt Johnson-Pint <mjp@hypermode.com>
2024-06-12 12:25:33 -0700

    Use class syntax

--- src/assembly/overrides/hypermode_console.ts
+++ src/assembly/overrides/hypermode_console.ts
@@ -5,69 +5,68 @@ declare function logToHypermode(level: string, message: string): void;
 // @ts-expect-error: decorator
 @lazy const timers = new Map<string, u64>();
 
-// eslint-disable-next-line @typescript-eslint/no-namespace
-export namespace hypermode_console {
-  export function assert<T>(condition: T, message: string = ""): void {
+export default abstract class hypermode_console {
+  static assert<T>(condition: T, message: string = ""): void {
     if (!condition) {
       logToHypermode("error", "Assertion failed: " + message);
     }
   }
 
-  export function log(message: string = ""): void {
+  static log(message: string = ""): void {
     logToHypermode("", message);
   }
 
-  export function debug(message: string = ""): void {
+  static debug(message: string = ""): void {
     logToHypermode("debug", message);
   }
 
-  export function info(message: string = ""): void {
+  static info(message: string = ""): void {
     logToHypermode("info", message);
   }
 
-  export function warn(message: string = ""): void {
+  static warn(message: string = ""): void {
     logToHypermode("warning", message);
   }
 
-  export function error(message: string = ""): void {
+  static error(message: string = ""): void {
     logToHypermode("error", message);
   }
 
-  export function time(label: string = "default"): void {
+  static time(label: string = "default"): void {
     const now = process.hrtime();
     if (timers.has(label)) {
       const msg = `Label '${label}' already exists for console.time()`;
-      warn(msg);
+      hypermode_console.warn(msg);
       return;
     }
     timers.set(label, now);
   }
 
-  export function timeLog(label: string = "default"): void {
+  static timeLog(label: string = "default"): void {
     const now = process.hrtime();
     if (!timers.has(label)) {
       const msg = `No such label '${label}' for console.timeLog()`;
-      warn(msg);
+      hypermode_console.warn(msg);
       return;
     }
-    timeLogImpl(label, now);
+    hypermode_console.timeLogImpl(label, now);
   }
 
-  export function timeEnd(label: string = "default"): void {
+  static timeEnd(label: string = "default"): void {
     const now = process.hrtime();
     if (!timers.has(label)) {
       const msg = `No such label '${label}' for console.timeEnd()`;
-      warn(msg);
+      hypermode_console.warn(msg);
       return;
     }
-    timeLogImpl(label, now);
+    hypermode_console.timeLogImpl(label, now);
     timers.delete(label);
   }
-}
 
-function timeLogImpl(label: string, now: u64): void {
-  const start = timers.get(label);
-  const nanos = now - start;
-  const millis = nanos / 1000000;
-  hypermode_console.log(`${label}: ${millis}ms`);
+  private static timeLogImpl(label: string, now: u64): void {
+    const start = timers.get(label);
+    const nanos = now - start;
+    const millis = nanos / 1000000;
+    hypermode_console.log(`${label}: ${millis}ms`);
+  }
 }
