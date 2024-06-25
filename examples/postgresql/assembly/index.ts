4bba1e01cf0a9097c1451d8703d1e15b9185280b
Matt Johnson-Pint <mjp@hypermode.com>
2024-06-25 10:08:30 -0700

    Update example

--- examples/postgresql/assembly/index.ts
+++ examples/postgresql/assembly/index.ts
@@ -1,18 +1,19 @@
 import { postgresql } from "@hypermode/functions-as";
 
+// The name of the PostgreSQL host, as specified in the hypermode.json manifest
+const host = "neon";
+
 class Person {
-  ID: i64 = 0;
+  id: i32 = 0;
   name!: string;
-  age!: i64;
+  age!: i32;
   home!: postgresql.Point;
 }
 
 export function queryPeople(): Person[] {
-  const resp = postgresql.query<Person>(
-    "neon",
-    "select name, age, home from people",
-  );
-  return resp.rows;
+  const query = "select name, age, home from people";
+  const response = postgresql.query<Person>(host, query);
+  return response.rows;
 }
 
 export function getPerson(name: string): Person | null {
@@ -21,20 +22,26 @@ export function getPerson(name: string): Person | null {
   const params: postgresql.Params = new postgresql.Params();
   params.push(name);
 
-  const people = postgresql.query<Person>("neon", query, params);
-  return people.rows[0];
+  const response = postgresql.query<Person>(host, query, params);
+  return response.rows[0];
 }
 
-export function addPerson(name: string, age: i32, lat: f64, lon: f64): void {
-  const query =
-    "insert into people (name, age, home) values ($1, $2, $3) RETURNING id";
+export function addPerson(name: string, age: i32, lat: f64, lon: f64): Person {
+  const query = `
+    insert into people (name, age, home)
+    values ($1, $2, $3) RETURNING id`;
 
   const params: postgresql.Params = new postgresql.Params();
   params.push(name);
   params.push(age);
   params.push(new postgresql.Point(lat, lon));
 
-  // TODO
-  const people = postgresql.query<Person>("neon", query, params);
-  const person = people.rows[0];
+  const response = postgresql.query<i32[]>(host, query, params);
+
+  if (response.rowsAffected !== 1) {
+    throw new Error("Failed to insert person.");
+  }
+
+  const id = response.rows[0][0];
+  return <Person>{ id, name, age, home: new postgresql.Point(lat, lon) };
 }
