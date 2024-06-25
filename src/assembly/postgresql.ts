548589642a88fd0ac8001642c9bf28c8677142a8
Matt Johnson-Pint <mjp@hypermode.com>
2024-06-25 09:57:03 -0700

    Make Point JSON serializable

--- src/assembly/postgresql.ts
+++ src/assembly/postgresql.ts
@@ -19,16 +19,68 @@ class hostResponse {
 }
 
 export class Point {
-  private X: f64;
-  private Y: f64;
-
-  constructor(X: f64, Y: f64) {
-    this.X = X;
-    this.Y = Y;
-  }
+  constructor(
+    public x: f64,
+    public y: f64,
+  ) {}
 
   public toString(): string {
-    return `(${this.X}, ${this.Y})`;
+    return `(${this.x}, ${this.y})`;
+  }
+
+  public static fromString(data: string): Point | null {
+    if (!data.startsWith("(") || !data.endsWith(")")) {
+      console.error(`Invalid Point string: "${data}"`);
+      return null;
+    }
+
+    const parts = data.substring(1, data.length - 1).split(",");
+    if (parts.length != 2) {
+      console.error(`Invalid Point string: "${data}"`);
+      return null;
+    }
+
+    const x = parseFloat(parts[0].trim());
+    const y = parseFloat(parts[1].trim());
+    return new Point(x, y);
+  }
+
+  // The following methods are required for custom JSON serialization
+  // This is used in lieu of the @json decorator, so that the class can be
+  // serialized to a string in PostgreSQL format.
+
+  __INITIALIZE(): this {
+    return this;
+  }
+
+  __SERIALIZE(): string {
+    return this.toString();
+  }
+
+  // @ts-ignore unused parameters
+  __DESERIALIZE(
+    data: string,
+    key_start: i32,
+    key_end: i32,
+    value_start: i32,
+    value_end: i32,
+  ): boolean {
+    if (
+      data.length < 7 ||
+      data.charAt(0) != '"' ||
+      data.charAt(data.length - 1) != '"'
+    ) {
+      return false;
+    }
+
+    const p = Point.fromString(data.substring(1, data.length - 1));
+    if (!p) {
+      return false;
+    }
+
+    this.x = p.x;
+    this.y = p.y;
+    return true;
   }
 }
 
